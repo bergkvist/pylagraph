2c28903cfa1d4792cb5424c5d2afd8091260013d
Tobias Bergkvist <tobias@bergkv.ist>
2022-03-02 05:32:50 +0100

    Create setup.cfg, pyproject.toml. Allow multiple library dirs when patching build_ext. Patch bdist_wheel to make wgeel platform specific, but not python specific

--- lagraph/__init__.py
+++ lagraph/__init__.py
@@ -1,34 +1,51 @@
 import sys
 import os
+from typing import List
 
 
-def get_include():
+def get_include() -> str:
     dirname = os.path.dirname(os.path.abspath(__file__))
     return os.path.abspath(os.path.join(dirname, '../lagraph.include'))
 
 
-def get_library_dir():
+def get_library_dir() -> str:
     dirname = os.path.dirname(os.path.abspath(__file__))
     return os.path.abspath(os.path.join(dirname, '../lagraph.libs'))
 
 
-def patch_build_ext(build_ext):    
+def patch_build_ext(build_ext, library_dirs):
     class patched_build_ext(build_ext):
         def build_extension(self, ext) -> None:
             super().build_extension(ext)
-            cmd = make_patch_rpath_command(platform=sys.platform,
-                                           ext_file=self.get_ext_fullpath(ext.name),
-                                           library_dir='lagraph.libs',
-                                           build_lib=self.build_lib)
-            print(cmd)
-            os.system(cmd)
+            cmds = make_patch_rpath_commands(
+                platform=sys.platform,
+                ext_file=self.get_ext_fullpath(ext.name),
+                library_dirs=library_dirs,
+                build_lib=self.build_lib
+            )
+            for cmd in cmds:
+                print(cmd)
+                os.system(cmd)
     return patched_build_ext
 
 
-def make_patch_rpath_command(platform: str, ext_file: str, build_lib: str, library_dir: str) -> str:
-    relpath = os.path.relpath(os.path.join(build_lib, library_dir), os.path.dirname(ext_file))
+def make_patch_rpath_commands(platform: str, ext_file: str, build_lib: str, library_dirs: List[str]) -> List[str]:
+    if len(library_dirs) == 0:
+        return []
+
+    rpaths = [ os.path.relpath(os.path.join(build_lib, library_dir), os.path.dirname(ext_file))
+               for library_dir in library_dirs ]
+
     if platform == 'linux':
-        return f"patchelf '{ext_file}' --set-rpath '$ORIGIN/{relpath}' --force-rpath"
+        rpath_string = ':'.join([ f'$ORIGIN/{r}' for r in rpaths ])
+        return [
+            f"patchelf '{ext_file}' --set-rpath '{rpath_string}' --force-rpath"
+        ]
+
     if platform == 'darwin':
-        return f"install_name_tool '{ext_file}' -add_rpath '@loader_path/{relpath}'"
+        return [
+            f"install_name_tool '{ext_file}' -add_rpath '@loader_path/{rpath}'"
+            for rpath in rpaths
+        ]
+
     raise Exception(f'Unsupported platform: {platform}')
--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 from setuptools import setup
 import os
+
+class bdist_wheel(_bdist_wheel):
+    def finalize_options(self):
+        super().finalize_options()
+        self.root_is_pure = False
+    def get_tag(self):
+        python, abi, platform =  super().get_tag()
+        python, abi = 'py3', 'none'
+        return python, abi, platform
 
 def files_in(dir: str):
     return [ os.path.join(dir, f) for f in os.listdir(dir) ]
 
 setup(
-    name='lagraph',
-    version='3.3.3',
-    description='Python wheel containing prebuild GraphBLAS/LAGraph libraries/header files.',
-    author='Tobias Bergkvist <tobias@bergkv.ist>',
     packages=['lagraph'],
     data_files=[
         ('../../lagraph.libs', files_in('./lagraph.libs')),
         ('../../lagraph.include', files_in('./lagraph.include'))
     ],
     zip_safe=False,
+    setup_requires=['patchelf; sys_platform == "linux"'],
+    cmdclass={'bdist_wheel': bdist_wheel}
 )
