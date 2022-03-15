e00e72922534c9dddf381e854628bdb73994118d
Tobias Bergkvist <tobias@bergkv.ist>
2022-03-15 11:21:34 +0100

    Update rpathtool to 0.3.0

--- rpathtool/setup.py
+++ rpathtool/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(
+  packages=['rpathtool'],
   install_requires=[
     'patchelf; sys_platform == "linux"'
     # install_name_tool on macOS (https://www.unix.com/man-page/osx/1/install_name_tool/)
